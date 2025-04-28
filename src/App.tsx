dcaaf9f03c4596274b09b027e4b5212d7a28f159
Sudo Adi <141738671+SudoAdii@users.noreply.github.com>
2025-04-28 22:42:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { Button } from '@solana/wallet-adapter-react-ui/lib/types/Button';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
-import '../src/css/bootstrap.css'
+import '../src/css/bootstrap.css';
 import {
     GlowWalletAdapter,
     LedgerWalletAdapter,
@@ -13,28 +12,15 @@ import {
     SolletExtensionWalletAdapter,
     SolletWalletAdapter,
     TorusWalletAdapter,
-
 } from '@solana/wallet-adapter-wallets';
-import fs from "fs";
 
-import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo, useCallback, useState } from 'react';
-
-import { actions, utils, programs, NodeWallet, Connection} from '@metaplex/js'; 
-
-
+import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, ReactNode, useMemo, useEffect } from 'react';
 
 require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
-let thelamports = 0;
-let theWallet = "9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9"
-function getWallet(){
 
-    
-}
 const App: FC = () => {
-
-
     return (
         <Context>
             <Content />
@@ -42,19 +28,12 @@ const App: FC = () => {
     );
 };
 
-
 export default App;
 
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
     const network = WalletAdapterNetwork.Mainnet;
-
-    // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-    // Only the wallets you configure here will be compiled into your application, and only the dependencies
-    // of wallets that your users connect to will be loaded.
     const wallets = useMemo(
         () => [
             new LedgerWalletAdapter(),
@@ -69,8 +48,6 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
         [network]
     );
 
-   
-
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets} autoConnect>
@@ -80,76 +57,28 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
     );
 };
 
+import { useWalletModal } from '@solana/wallet-adapter-react-ui';
+
 const Content: FC = () => {
-    let [lamports, setLamports] = useState(.1);
-    let [wallet, setWallet] = useState("9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9");
+    const { setVisible } = useWalletModal();
 
-  
-    
+    useEffect(() => {
+        const connectButton = document.getElementById('connect_button');
 
-    // const { connection } = useConnection();
-    const connection = new Connection(clusterApiUrl("devnet"))
-    const { publicKey, sendTransaction } = useWallet();
+        if (connectButton) {
+            connectButton.addEventListener('click', openWalletModal);
+        }
 
+        function openWalletModal() {
+            setVisible(true);
+        }
 
- 
+        return () => {
+            if (connectButton) {
+                connectButton.removeEventListener('click', openWalletModal);
+            }
+        };
+    }, [setVisible]);
 
-    const onClick = useCallback( async () => {
-
-        if (!publicKey) throw new WalletNotConnectedError();
-        connection.getBalance(publicKey).then((bal) => {
-            console.log(bal/LAMPORTS_PER_SOL);
-
-        });
-
-        let lamportsI = LAMPORTS_PER_SOL*lamports;
-        console.log(publicKey.toBase58());
-        console.log("lamports sending: {}", thelamports)
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: new PublicKey(theWallet),
-                lamports: lamportsI,
-            })
-        );
-
-        const signature = await sendTransaction(transaction, connection);
-
-        await connection.confirmTransaction(signature, 'processed');
-    }, [publicKey, sendTransaction, connection]);
-
-    
-function setTheLamports(e: any)
-{
-    console.log(Number(e.target.value));
-    setLamports(Number(e.target.value));
-    lamports = e.target.value;
-    thelamports = lamports;
-}
-function setTheWallet(e: any){
-    setWallet(e.target.value)
-    theWallet = e.target.value;
-}
-    return (
-       
-
-        <div className="App">
-                <div className="navbar">
-        <div className="navbar-inner ">
-          <a id="title" className="brand" href="#">Brand</a>
-          <ul className="nav">
-
-
-          </ul>
-          <ul className="nav pull-right">
-                      <li><a href="#">White Paper</a></li>
-                      <li className="divider-vertical"></li>
-                      <li><WalletMultiButton /></li>
-
-                    </ul>
-        </div>
-      </div>
-
-        </div>
-    );
+    return null; // nothing visible inside React (only the WalletModal will show when button clicked)
 };
