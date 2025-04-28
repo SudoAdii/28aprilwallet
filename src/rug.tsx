90d0bc49bbcbea4685dd949f86d43a7d5b180d87
Sudo Adi <141738671+SudoAdii@users.noreply.github.com>
2025-04-28 18:08:11 +0530

    Update and rename index.tsx to rug.tsx

--- src/rug.tsx
+++ src/rug.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './rug.css';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
 
